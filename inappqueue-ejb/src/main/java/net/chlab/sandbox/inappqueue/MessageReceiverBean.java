abd385941dda0e6c58cefc4c7b7ffd4a62f08604
Christian Heinemann <ch@chlab.net>
2014-01-26 17:13:52 +0100

    Simple application which creates a message queue at deployment

---
+++ inappqueue-ejb/src/main/java/net/chlab/sandbox/inappqueue/MessageReceiverBean.java
@@ -0,0 +1,15 @@
+package net.chlab.sandbox.inappqueue;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/InAppQueue") })
+public class MessageReceiverBean implements MessageListener {
+
+    public void onMessage(Message message) {
+    }
+}
