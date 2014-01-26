925ff4d3f3654758325c833af2058514dcdaea90
Christian Heinemann <ch@chlab.net>
2014-01-26 21:31:04 +0100

    Move the MDB activation config to JBoss deployment descriptor

--- inappqueue-ejb/src/main/java/net/chlab/sandbox/inappqueue/MessageReceiverBean.java
+++ inappqueue-ejb/src/main/java/net/chlab/sandbox/inappqueue/MessageReceiverBean.java
@@ -1,13 +1,10 @@
 package net.chlab.sandbox.inappqueue;
 
-import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
-@MessageDriven(activationConfig = {
-        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
-        @ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/InAppQueue") })
+@MessageDriven
 public class MessageReceiverBean implements MessageListener {
 
     public void onMessage(Message message) {
